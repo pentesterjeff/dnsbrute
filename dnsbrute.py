c22e4c888b9af038c9c4db1c41f4e79667da5bb9
pentesterjeff <36642753+pentesterjeff@users.noreply.github.com>
2018-02-19 15:38:56 -0300

    Add files via upload

---
+++ dnsbrute.py
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+import dns.resolver
+import sys
+
+try:
+	domin = sys.argv[1]
+	name_arq = sys.argv[2]
+except:
+	print 'Error'
+	print 'Usage: dnsbrute.py <URL> <WORDLIST>'
+	sys.exit(1)
+try:
+	arq = open(name_arq)
+	lines = arq.read().splitlines()
+except:
+	print 'File Not Found'
+	sys.exit(1)
+
+for subdom in lines:
+	try:
+		domesub = subdom + '.' + domin
+		r = dns.resolver.query(domesub, 'a')
+		for result in r:
+			print domesub, result
+	except:
+		pass
